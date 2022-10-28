54c98f8cae13f10b7fb0a0b33a10445325ad1149
raykong06 <114007162+raykong06@users.noreply.github.com>
2022-10-28 14:47:57 -0400

    Update CircularBinary.java

--- src/CircularBinary.java
+++ src/CircularBinary.java
@@ -4,22 +4,16 @@ public class CircularBinary {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int number;
-        double calcNumber;
         int power = 0;
         String binary;
         int remaining;
-        int index = 0;
-        int length;
 
         System.out.println("Welcome to the Circular Problem Solver!");
         System.out.print("How many numbers are in the circle? (n): ");
         number = s.nextInt();
         binary = Integer.toBinaryString(number);
-        calcNumber = number;
 
-        length = binary.length();
-
-power = Integer.parseInt(binary.substring(length - 1));
+        power = binary.length() - 1;
 
         remaining = (number - (int)Math.pow(2, power)) * 2 + 1;
 
